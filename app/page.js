3537ba5ee4774fc156a5f3442a7c8c4c461b39c0
Kuwar Chahal <kowarjitsingh.chahal@edu.sait.ca>
2024-02-06 09:23:33 -0700

    Week 3

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Link from "next/link"
+
+export default function Home() {
+  return (
+    <main>
+      <div className="">
+        <h1 className="text-4xl font-bold mb-5">CPRG 306: Web Development 2 - Assignments</h1>
+          <Link  href="/week-2">Week-2 Assignment</Link>
+          <br/>
+          <Link href="/week-3">Week-3 Assignment</Link>
+      </div>
+    </main>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function StudentInfo(){
+    return(
+        <div>
+            <div>Kowarjit Chahal</div>
+            <Link href="https://github.com/KuwarChahal/cprg306-assignments">Github link</Link>
+        </div>
+    );
+}
